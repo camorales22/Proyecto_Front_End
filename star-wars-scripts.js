5e4c16b500982b6c48d2a4299fb348cd4fb7d5d7
camorales22 <camorales22@poligran.edu.co>
2020-04-19 19:20:46 -0500

    Star Wars App

---
+++ star-wars-scripts.js
@@ -0,0 +1,86 @@
+﻿
+/* busqueda de personajes */
+function FuncionPersonajes(){
+var temp = document.querySelector('ul');
+temp.style.color = "white";
+fetch('https://swapi.py4e.com/api/people') 
+.then(function(response) {   
+  return response.json(); 
+})
+.then(function(FPersonajes) {
+  let Personajes = FPersonajes.results; 
+
+  for(p of Personajes) {
+    
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    temp.appendChild(listItem);
+       
+  }
+
+});
+}
+/* busqueda de planetas*/
+function FuncionPlanetas(){
+var temp = document.querySelector('ul');
+temp.style.color = "white";
+fetch('https://swapi.py4e.com/api/planets') 
+.then(function(response) {   
+  return response.json(); 
+})
+.then(function (FPlanetas) {
+  let Planetas = FPlanetas.results; 
+
+  for(p of Planetas) {
+    
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    temp.appendChild(listItem);  
+  }
+
+});
+} 
+
+/* busqueda de peliculas*/
+function FuncionPeliculas(){
+  var temp = document.querySelector('ul');
+  temp.style.color = "white";
+  fetch('https://swapi.py4e.com/api/films') 
+  .then(function(response) {   
+    return response.json(); 
+  })
+  .then(function (FPeliculas) {
+    let Peliculas = FPeliculas.results; 
+  
+    for(p of Peliculas) {
+      
+      let listItem = document.createElement('li'); 
+      listItem.innerHTML = '<p>' + p.title + '</p>'; 
+      temp.appendChild(listItem);  
+    }
+  
+  });
+  } 
+  
+/* busqueda de peliculas*/
+function FuncionNaves(){
+  var temp = document.querySelector('ul');
+  temp.style.color = "white";
+  fetch('https://swapi.py4e.com/api/starships') 
+  .then(function(response) {   
+    return response.json(); 
+  })
+  .then(function (FNaves) {
+    let Naves = FNaves.results; 
+  
+    for(p of Naves) {
+      
+      let listItem = document.createElement('li'); 
+      listItem.innerHTML = '<p>' + p.name + '</p>'; 
+      temp.appendChild(listItem);  
+    }
+    });
+  } 
+function FuncionLimpiar(elementID){
+  document.getElementById(elementID).innerText = ""; 
+}
